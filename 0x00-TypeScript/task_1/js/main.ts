2287ed1932339c7de3311a90bf8931fdeb4baafc
Boul3ez85 <benrebah.moez@gmail.com>
2021-10-26 18:57:03 +0100

    create Student class

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -16,3 +16,33 @@ interface printTeacherFunction {
 };
 
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName.charAt(0)}. ${lastName}`;
+
+interface StudentConstructor {
+  (firstName: string, lastName: string): Student;
+};
+
+class StudentClass implements Student {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  };
+
+  workOnHomework(): string {
+    return "Currently working";
+  };
+
+  displayName(): string {
+    return this.firstName;
+  };
+};
+
+interface Student {
+  workOnHomework(): string;
+  displayName(): string;
+};
+
+const student = new StudentClass('Moez', 'Ben Rebah');
+console.log(student.displayName() + " " + student.workOnHomework());
