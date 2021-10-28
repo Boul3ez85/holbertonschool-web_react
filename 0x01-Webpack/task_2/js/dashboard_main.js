ef74a00f18d3f360a301ef88656f3b9fa0da5f09
Boul3ez85 <benrebah.moez@gmail.com>
2021-10-28 17:27:01 +0100

    add css&image

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,21 @@
+import '../css/main.css';
+import $ from 'jquery';
+import _ from 'lodash';
+
+$('body').append("<div id='logo'></div>");
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="#count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+const updateCounter = () => {
+  let counter = $('#count').html() || 0;
+  $('button').on('click', () => {
+    counter++;
+    $('#count').html(`${counter} clicks on the button`);
+  });
+};
+  
+_.debounce(updateCounter, 500);
+updateCounter();
