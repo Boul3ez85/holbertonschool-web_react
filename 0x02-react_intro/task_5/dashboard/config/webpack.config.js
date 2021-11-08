7104a366218400dce31c9bfb5278e72ddbf379d2
Boul3ez85 <benrebah.moez@gmail.com>
2021-11-08 01:38:52 +0100

    update dependencies

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,13 +1,20 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
+        devtool: 'inline-source-map',
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
@@ -32,8 +39,10 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Webpack Output",
+      template: './dist/index.html',
+      title: "Holberton Dashboard",
     }),
   ],
 };
