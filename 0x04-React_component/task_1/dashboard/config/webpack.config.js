989b27010658a8a514be26bf4824e27cc0476d93
Boul3ez85 <benrebah.moez@gmail.com>
2021-11-16 13:29:13 +0100

    implement logout function

---
+++ 0x04-React_component/task_1/dashboard/config/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/App/App.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+	hot: true,
+	static: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+	new CleanWebpackPlugin(),
+	new HtmlWebpackPlugin({
+	  template: './dist/index.html',
+      title: 'Holberton Dashboard',
+    }),
+  ],
+  module: {
+	rules: [
+	  {
+		test: /\.(js|jsx)$/,
+		exclude: /node_modules/,
+		loader: 'babel-loader',
+		options: {
+			presets: ['@babel/preset-env', '@babel/react']
+		}
+	  },
+	  {
+		test: /\.css$/i,
+		use: ['style-loader', 'css-loader'],
+	  },
+	  {
+		test: /\.(png|svg|jpg|jpeg|gif)$/i,
+		type: 'asset/resource',
+		loader: 'image-webpack-loader',
+  	  },
+	],
+  },
+};
