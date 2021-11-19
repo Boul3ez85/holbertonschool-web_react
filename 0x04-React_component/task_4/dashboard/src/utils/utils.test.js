f10a035959121fd5d424311c21107c01721018b4
Boul3ez85 <benrebah.moez@gmail.com>
2021-11-19 08:29:48 +0100

    HOC tests

---
+++ 0x04-React_component/task_4/dashboard/src/utils/utils.test.js
@@ -0,0 +1,27 @@
+import { getFullYear, getFooterCopy, getLatestNotification } from './utils.js';
+
+describe('getFullYear', () => {
+  test('the year rendered', () => {
+    expect(getFullYear()).toEqual(new Date().getFullYear());
+  });
+});
+
+describe('getFooterCopy', () => {
+  test('the string rendered', () => {
+    expect(getFooterCopy(true)).toEqual('Holberton School');
+  });
+});
+
+describe('getFooterCopy', () => {
+    test('the string rendered', () => {
+      expect(getFooterCopy(false)).toEqual('Holberton School main dashboard');
+    });
+  });
+
+describe('getLatestNotification', () => {
+  test('the notification rendered', () => {
+    expect(getLatestNotification()).toEqual(
+        '<strong>Urgent requirement</strong> - complete by EOD'
+      );
+  });
+});
