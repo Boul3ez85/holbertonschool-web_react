5b8dd534b5b0be259403197f08864c7ea2dc2b2f
Boul3ez85 <benrebah.moez@gmail.com>
2021-11-21 12:51:52 +0100

    inline style

--- 0x05-React_inline_styling/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
+++ 0x05-React_inline_styling/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -7,21 +7,37 @@ import BodySectionWithMarginBottom from './BodySectionWithMarginBottom';
 
 configure({adapter: new Adapter()});
 
-describe("Testing the <BodySectionWithMarginBottom /> Component", () => {
-	it("Renders the 'BodySection' Component correctly", () => {
-		let props = {
-			title: 'title',
-			children: React.createElement('p', 'test child'),
-		};
+describe("<BodySectionWithMarginBottom />", () => {
 
-		let wrapper = shallow(
-			<BodySectionWithMarginBottom {...props} />
-		);
+  it("BodySectionWithMarginBottom renders without crashing", () => {
 
-		expect(wrapper.containsAllMatchingElements([
-			<div className="BodySectionWithMargin">
-				<BodySection {...props} />
-			</div>
-		])).to.equal(true);
-	});
+    const wrapper = shallow(<BodySectionWithMarginBottom />);
+    expect(wrapper.exists()).to.be.equal(true);
+  });
+
+  it("Shallowing the component should render correctly a BodySection component and that the props are passed correctly to the child component", () => {
+    const wrapper = shallow(
+      <BodySectionWithMarginBottom title="test title">
+        <p>test children node</p>
+      </BodySectionWithMarginBottom>
+    );
+
+    const BodySection = wrapper.find("BodySection");
+
+    expect(BodySection).to.have.lengthOf(1);
+    expect(BodySection.props().title).to.be.equal("test title");
+
+    const internalBody = BodySection.dive();
+
+    const h2 = internalBody.find("h2");
+    const p = internalBody.find("p");
+
+    expect(h2).to.have.lengthOf(1);
+    expect(h2.text()).to.be.equal("test title");
+
+    expect(p).to.have.lengthOf(1);
+    expect(p.text()).to.be.equal("test children node");
+  });
+
 });
+
--- 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -3,24 +3,34 @@ import PropTypes from 'prop-types';
 
 class CourseListRow extends React.Component {
   render() {
+
     let { isHeader, textFirstCell, textSecondCell } = this.props;
-    let backgroundRowColor = isHeader ? { "backgroundColor": "#deb5b545" } : { "backgroundColor": "#f5f5f5ab" }
+
+    const rowBackgroundColor = {"background": "#f5f5f5ab"};
+    const headerBackgroundColor = {"backgroundColor": "#deb5b545"}
+    let changeColor;
+    if (isHeader) {
+      changeColor = headerBackgroundColor;
+    } else {
+      changeColor = rowBackgroundColor;
+    }
+
     if (isHeader === true && textSecondCell == null) {
       return (
-        <tr style={backgroundRowColor}>
+        <tr style={changeColor}>
           <th colSpan={2}>{textFirstCell}</th>
         </tr>
       );
     } else if (isHeader === true && textSecondCell != null) {
       return (
-        <tr style={backgroundRowColor}>
+        <tr style={changeColor}>
           <th>{textFirstCell}</th>
           <th>{textSecondCell}</th>
         </tr>
       );
     } else if (isHeader === false) {
       return (
-        <tr style={backgroundRowColor}>
+        <tr style={changeColor}>
           <td>{textFirstCell}</td>
           <td>{textSecondCell}</td>
         </tr>
